fe396466b52fbff62a4de1b80f54de596d412f0a
lSub02l <bazylipiotr@gmail.com>
2023-12-15 22:15:59 +0100

    implemented test code for creating data

--- web-scraper.py
+++ web-scraper.py
@@ -1,8 +1,29 @@
 from selenium import webdriver
+import gspread
+
+AUTH_KEY = "C://Users//mrzeq//Downloads//googleauth.json"
 
 # Selenium driver init
 driver = webdriver.Chrome()
 url = "https://www.apartments.com/new-york-ny/800-to-2500/"
 driver.get(url)
+
+# Downloading elements
+listings = driver.find_elements
+
+# Google sheets init
+gc = gspread.service_account(filename=AUTH_KEY)
+sh = gc.open('renting_data')
+worksheet = sh.sheet1
+
+# Saving data 
+row = 1
+for listing in listings:
+    address = listing.find_element_by_class_name("propertyAddress").text.strip()
+    price = listing.find_element_by_class_name("rentPrice").text.strip()
+
+    worksheet.update(f'A{row}', address)
+    worksheet.update(f'B{row}', price)
+    row += 1
 
 driver.quit()
