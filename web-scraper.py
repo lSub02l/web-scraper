b119b87de1a41f0b3ca3a16c883dad9e0fb76d8a
Zeqterr <bazylipiotrek@gmail.com>
2023-12-24 10:24:55 +0100

    implemented new search but not listed yet

--- web-scraper.py
+++ web-scraper.py
@@ -1,29 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import gspread
 
-AUTH_KEY = "C://Users//mrzeq//Downloads//googleauth.json"
+#AUTH_KEY = "C://Users//mrzeq//Downloads//googleauth.json"
+AUTH_KEY = "C://Users//bazyl//Desktop//googleauth.json"
 
 # Selenium driver init
 driver = webdriver.Chrome()
 url = "https://www.apartments.com/new-york-ny/800-to-2500/"
 driver.get(url)
 
-# Downloading elements
-listings = driver.find_elements_by_class_name("name")
-
 # Google sheets init
 gc = gspread.service_account(filename=AUTH_KEY)
 sh = gc.open('renting_data')
 worksheet = sh.sheet1
 
-# Saving data 
-row = 1
-for listing in listings:
-    address = listing.find_element_by_class_name("propertyAddress")
-    price = listing.find_element_by_class_name("rentPrice")
+listings = driver.find_elements("placard")
 
-    worksheet.update(f'A{row}', address)
-    worksheet.update(f'B{row}', price)
-    row += 1
+# Saving data 
+for listing in listings:
+    address_element = listing.find_element_by_css_selector(".property-address.js-url")
+    address = address_element.text.strip()
+
+    price_element = listing.find_element_by_class_name("property-rents")
+    price = price_element.text.strip()
+
+    beds_element = listing.find_element_by_class_name("property-beds")
+    beds = beds_element.text.strip()
+
+    print(f"Address: {address}\nPrice: {price}\nBeds: {beds}\n")
 
 driver.quit()
